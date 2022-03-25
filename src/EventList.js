7373d15aa2fa8b2907741100bd7fb2fae12f96a9
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-03-25 15:10:41 +0100

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -3,11 +3,11 @@ import Event from './Event';
 
 class EventList extends Component {
   render() {
-    const adjustedEventList = this.props.events.splice(0, this.props.number);
+    const { events } = this.props;
 
     return (
       <ul className="EventList">
-        {adjustedEventList.map((event) => (
+        {events.map((event) => (
           <li key={event.id}>
             <Event event={event} />
           </li>
