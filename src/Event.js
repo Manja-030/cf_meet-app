5451b4dead7e8f8674512b192a1a80bd536ab528
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-04-02 11:35:44 +0200

    add Google Calendar link

--- src/Event.js
+++ src/Event.js
@@ -29,7 +29,12 @@ class Event extends Component {
         {!collapsed && (
           <div className="details event-text">
             <h3 className="event-text">About event:</h3>
-            <a href={event.htmlLink} target="_blank" rel="noreferrer">
+            <a
+              className="calendar-link"
+              href={event.htmlLink}
+              target="_blank"
+              rel="noreferrer"
+            >
               See details on Google Calendar
             </a>
             <p className="description event-text">{event.description}</p>
