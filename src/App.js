675a108876252b5635d94cdc6a7347e208d53831
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-04-03 14:01:57 +0200

    add WelcomeScreen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './App.css';
 import './nprogress.css';
 import meet_logo2 from './assets/meet_logo2.jpg';
@@ -8,6 +8,7 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { InfoAlert } from './Alert';
 import { BsWifiOff } from 'react-icons/bs';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   state = {
@@ -15,18 +16,26 @@ class App extends Component {
     locations: [],
     number: 10,
     location: 'all',
+    showWelcomeScreen: undefined,
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, this.state.number),
-          locations: extractLocations(events),
-        });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.number),
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -59,6 +68,8 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
       <div className="App">
         <div className="header">
@@ -91,6 +102,12 @@ class App extends Component {
         </div>
 
         <EventList events={this.state.events} number={this.state.number} />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
       </div>
     );
   }
