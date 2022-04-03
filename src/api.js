94ebe8c360432794f9500b4c885f1270f12acf81
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-04-03 10:09:20 +0200

    Revert "add caching of event data"
    This reverts commit e1250dc29f677819e681beb3cc25d6ae21552391.

--- src/api.js
+++ src/api.js
@@ -39,11 +39,6 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
-  if (!navigator.onLine) {
-    const data = localStorage.getItem('lastEvents');
-    NProgress.done();
-    return data ? JSON.parse(data).events : [];
-  }
 
   const token = await getAccessToken();
 
