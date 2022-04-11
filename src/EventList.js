8ae091d04d9ea58086278de55c5600a12580af6b
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-04-11 14:49:58 +0200

    try to debug

--- src/EventList.js
+++ src/EventList.js
@@ -4,11 +4,14 @@ import Event from './Event';
 class EventList extends Component {
   render() {
     const { events } = this.props;
+    const numberFilter = events.filter((item, index) => {
+      return index < this.props.number;
+    });
     return (
       <div>
         <p>Hello</p>
         <ul className="EventList">
-          {events.map((event) => {
+          {numberFilter.map((event) => {
             return (
               <li key={event.id}>
                 <Event event={event} />
