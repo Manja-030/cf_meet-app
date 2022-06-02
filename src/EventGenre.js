1f826f140316716e04763472e5dc6c11b01ad691
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-06-02 19:49:26 +0200

    add line break to pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -36,7 +36,7 @@ const EventGenre = ({ events }) => {
           dataKey="value"
           label={({ name, percent }) => {
             if (percent > 0) {
-              return `${name}
+              return `${name}\n
               ${(percent * 100).toFixed(0)}%`;
             }
           }}
