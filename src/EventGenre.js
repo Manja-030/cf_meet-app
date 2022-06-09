fecaf6aeeeea55dd5f3a65ff5a734239a194df34
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-06-09 19:18:39 +0200

    remove line break

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -36,7 +36,7 @@ const EventGenre = ({ events }) => {
           dataKey="value"
           label={({ name, percent }) => {
             if (percent > 0) {
-              return `${name}\n${(percent * 100).toFixed(0)}%`;
+              return `${name} ${(percent * 100).toFixed(0)}%`;
             }
           }}
         >
